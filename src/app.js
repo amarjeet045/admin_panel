f15044495873c6e83688345adbf4a08607ffa312
shikhar0507 <kap.shikhar@gmail.com>
2019-03-18 12:21:39 +0530

    dist ready for prod

--- src/app.js
+++ src/app.js
@@ -2,10 +2,8 @@ import * as login from './assests/js/login';
 
 const config = {
   apiKey: 'AIzaSyA4s7gp7SFid_by1vLVZDmcKbkEcsStBAo',
-  authDomain: 'growthfile-207204.firebaseapp.com',
-  databaseURL: 'https://growthfile-207204.firebaseio.com',
+  authDomain: 'growthfile-adminpanel.firebaseapp.com',
   projectId: 'growthfile-207204',
-  storageBucket: 'growthfile-207204.appspot.com',
   messagingSenderId: '701025551237'
 }
 
