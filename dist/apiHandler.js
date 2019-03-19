066200e4afe1503afe4c5cd904f20b4e2e3f3ed3
shikhar0507 <kap.shikhar@gmail.com>
2019-03-19 15:46:13 +0530

    Added apiHandler.js in dist

---
+++ dist/apiHandler.js
@@ -0,0 +1,398 @@
+importScripts('https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.18.1/moment.js')
+// Backend API Url
+
+const functionCaller = {
+  search: search,
+  create: create,
+  read: read,
+  fetchServerTime: fetchServerTime,
+}
+
+self.onmessage = function (event) {
+  console.log(event)
+  functionCaller[event.data.type](event.data).then(function (response) {
+    self.postMessage({
+      success: true,
+      message: response
+    });
+  }).catch(console.log)
+}
+
+function http(method, url, data) {
+  return new Promise(function (resolve, reject) {
+    const xhr = new XMLHttpRequest()
+    xhr.open(method, url, true)
+    xhr.setRequestHeader('X-Requested-With', 'XMLHttpRequest')
+    xhr.setRequestHeader('Content-Type', 'application/json')
+    xhr.setRequestHeader('Authorization', data.idToken)
+    xhr.onreadystatechange = function () {
+      if (xhr.readyState === 4) {
+        if (!xhr.status) {
+          return reject(xhr);
+        }
+        if (xhr.status > 226) {
+          return reject(xhr.response)
+        }
+        return resolve(JSON.parse(xhr.responseText))
+      }
+    }
+    if (method == 'GET') {
+      xhr.send(null)
+    } else {
+      xhr.send(JSON.stringify(data.body))
+    }
+  })
+}
+
+function fetchServerTime(data) {
+  return new Promise((resolve, reject) => {
+    let url = `${data.baseUrl}admin/now?deviceId=${data.body.id}`;
+    data.claims.support ? url.concat('&support=true') : ''
+    http(
+      'GET',
+      url,
+      data
+    ).then(function (response) {
+      console.log(response)
+      initializeIDB(data.uid, response.timestamp).then(function (uid) {
+        resolve(uid);
+      }, function (error) {
+        reject({
+          success: false,
+          message: error
+        })
+      })
+    }, function (error) {
+      self.postMessage({
+        success: false,
+        message: error
+      })
+    })
+  })
+}
+
+function search(data) {
+  return new Promise((resolve, reject) => {
+    let url = `${data.baseUrl}admin/search?query=${data.body.office}`
+    data.claims.support ? url.concat('&support=true') : ''
+    http(
+      'GET',
+      url,
+      data
+    ).then(function (response) {
+      console.log(response)
+      resolve(response)
+    }).catch(function (error) {
+      reject(error)
+    })
+  })
+}
+
+function create(data) {
+  return new Promise((resolve, reject) => {
+    let url = `${data.baseUrl}admin/bulk`
+    data.claims.support ? url.concat('?support=true') : ''
+    http(
+      'PUT',
+      url,
+      data
+    ).then(function (success) {
+      console.log(success)
+      resolve(success)
+    }).catch(function (error) {
+      reject(error)
+    })
+  })
+}
+
+function initializeIDB(uid, serverTime) {
+  console.log("init db")
+  // onAuthStateChanged is added because app is reinitialized
+  // let hasFirstView = true
+  return new Promise(function (resolve, reject) {
+
+    const request = indexedDB.open(uid, 1);
+
+    request.onerror = function (event) {
+      reject(event.error);
+    }
+
+    request.onupgradeneeded = function () {
+      const db = request.result
+
+      const activities = db.createObjectStore('activities', {
+        keyPath: 'activityId'
+      })
+      activities.createIndex('list', ['canEditRule', 'office', 'template']);
+
+
+      const users = db.createObjectStore('users', {
+        keyPath: 'phoneNumber'
+      })
+      users.createIndex('name', 'displayName');
+      users.createIndex('updated', 'updated');
+      const templates = db.createObjectStore('templates', {
+        autoIncrement: true,
+
+      })
+      templates.createIndex('selectTemplate', ['canEditRule', 'office']);
+      templates.createIndex('office', 'office');
+      templates.createIndex('template', 'name');
+      templates.createIndex('selectDetail', ['canEditRule', 'office', 'name']);
+      templates.createIndex('officeTemplate', ['office', 'name']);
+      // const officeValidation = db.createObjectStore('officeValidation',{
+      //   autoIncrement:true
+      // })
+      // officeValidation.createIndex('name','Name',{unique:true});
+      // officeValidation.createIndex('FirstContact','FirstContact');
+      // officeValidation.createIndex('SecondContact','SecondContact');
+      const root = db.createObjectStore('root', {
+        keyPath: 'uid'
+      })
+      root.put({
+        uid: uid,
+        fromTime: 0,
+        location: {
+          latitude: '',
+          longitude: '',
+          accuracy: '',
+          lastLocationTime: '',
+          provider: ''
+        }
+      });
+
+    }
+
+    request.onsuccess = function () {
+
+      const rootTx = request.result.transaction(['root'], 'readwrite')
+
+      const rootObjectStore = rootTx.objectStore('root')
+      rootObjectStore.get(uid).onsuccess = function (event) {
+        const record = event.target.result
+        record.serverTime = serverTime - Date.now()
+        rootObjectStore.put(record)
+      }
+
+      rootTx.oncomplete = function () {
+        resolve(uid)
+      }
+    }
+  })
+}
+
+const getFromTime = (data) => {
+  return new Promise((resolve, reject) => {
+    const uid = data.uid
+    const req = indexedDB.open(data.uid)
+
+    req.onsuccess = function () {
+      const db = req.result
+      const rootObjectStore = db.transaction('root', 'readonly').objectStore('root')
+
+      rootObjectStore.get(uid).onsuccess = function (event) {
+        let fromTime;
+        const root = event.target.result;
+        if (root.hasOwnProperty(data.body.office)) {
+          fromTime = root[data.body.office]
+          return resolve(fromTime);
+        }
+        return resolve(0);
+      }
+    }
+  });
+}
+
+function read(data) {
+  return new Promise((resolve, reject) => {
+    getFromTime(data).then(function (fromTime) {
+      let url = `${data.baseUrl}admin/read?from=${fromTime}&office=${data.body.office}`
+      data.claims.support ? url.concat('&support=true') : ''
+      http(
+          'GET',
+          url,
+          data
+        )
+        .then(function (response) {
+          successResponse(response, data).then(function (response) {
+            resolve(response)
+          }).catch(function (error) {
+            reject(error)
+          })
+        }).catch(function (error) {
+          reject(error);
+        })
+    })
+  })
+}
+
+function successResponse(read, data) {
+  return new Promise((resolve, reject) => {
+
+
+    console.log(read)
+    const req = indexedDB.open(data.uid);
+    req.onsuccess = function () {
+      const db = req.result;
+      const transaction = db.transaction(['activities', 'users', 'root'], 'readwrite')
+      const activityStore = transaction.objectStore('activities');
+      const length = read.activities.length
+      for (let index = length; index--;) {
+        const activity = read.activities[index];
+        activityStore.put(activity);
+      }
+
+
+      const rootObjectStore = transaction.objectStore('root');
+      rootObjectStore.get(data.uid).onsuccess = function (event) {
+        const record = event.target.result;
+        delete record['fromTime'];
+        record[data.body.office] = read.upto;
+        rootObjectStore.put(record);
+      }
+
+      transaction.oncomplete = function () {
+        updateTemplates(read.templates, data).then(function () {
+          resolve(true);
+        }).catch(function (error) {
+          console.log(error)
+          reject(error)
+        })
+
+      }
+      transaction.onerror = function () {
+        reject(transaction.error.message);
+      }
+    }
+  });
+}
+
+const addUsers = (activity, transaction) => {
+  const userStore = transaction.objectStore('users');
+  activity.adminsCanEdit.forEach(function (user) {
+    userStore.get(user).onsuccess = function (event) {
+      const record = event.target.result;
+      if (!record) {
+        userStore.put({
+          phoneNumber: user,
+          photoURL: '',
+          displayName: '',
+          lastSignInTime: '',
+
+        });
+      }
+    }
+  })
+}
+
+const createUsersApiRequest = (data) => {
+  return new Promise((resolve, reject) => {
+    let string = '';
+    let baseurl = `${apiUrl}services/users?q=`;
+    const req = indexedDB.open(data.uid);
+    req.onsuccess = function () {
+      const db = req.result;
+      const transaction = db.transaction(['users'], 'readwrite');
+      const userStore = transaction.objectStore('users');
+      userStore.openCursor(null, 'next').onsuccess = function (event) {
+        const cursor = event.target.result;
+        if (!cursor) return;
+        string += `%2B${cursor.value.phoneNumber.replace('+', '')}&q=`
+        cursor.continue();
+      }
+
+      transaction.oncomplete = function () {
+        baseurl = baseurl + string;
+        http('GET', baseurl, data).then(function (result) {
+          resolve(updateUsers(result, data));
+        }).catch(console.log)
+      }
+
+      transaction.onerror = function () {
+        reject(transaction.error)
+      }
+    }
+    req.onerror = function () {
+      reject(req.error)
+    }
+  })
+}
+
+let updateTemplates = (templates, data) => {
+  return new Promise((resolve, reject) => {
+    const req = indexedDB.open(data.uid);
+    req.onsuccess = function () {
+      const db = req.result;
+      const deletTransaction = db.transaction(['templates'], 'readwrite');
+      const store = deletTransaction.objectStore('templates');
+      const index = store.index('officeTemplate');
+
+      templates.forEach(function (template) {
+        index.openCursor([data.body.office, template.name]).onsuccess = function (event) {
+          const cursor = event.target.result;
+          if (cursor) {
+            console.log(cursor);
+            const deleteReq = cursor.delete();
+            deleteReq.onsuccess = function () {
+              console.log('deleted record')
+            }
+            deleteReq.onerror = function () {
+              console.log(deleteReq.error)
+            }
+          } 
+        }
+      });
+      deletTransaction.oncomplete = function(){
+        console.log('finished deleting')
+        const addReq = indexedDB.open(data.uid);
+        addReq.onsuccess = function(){
+          const addDb = addReq.result;
+          const addTx = addDb.transaction(['templates'],'readwrite');
+          const addStore = addTx.objectStore('templates');
+          templates.forEach(function(template){
+            template.office = data.body.office
+            addStore.put(template)
+          })
+          addTx.oncomplete = function(){
+            console.log('completed')
+            resolve(true)
+          }
+        }
+      }
+    }
+  })
+}
+
+let updateUsers = (result, data) => {
+  return new Promise((resolve, reject) => {
+
+    const req = indexedDB.open(data.uid);
+    req.onsuccess = function () {
+      const db = req.result;
+      const transaction = db.transaction(['users'], 'readwrite');
+      const userStore = transaction.objectStore('users');
+      userStore.openCursor().onsuccess = function (event) {
+        const cursor = event.target.result;
+        if (!cursor) return;
+
+        const value = result[cursor.value.phoneNumber];
+        // rec = result[cursor.value.phoneNumber];
+        if (value.displayName && value.photoURL) {
+          cursor.value.photoURL = value.photoURL;
+          cursor.value.displayName = value.displayName;
+        }
+        userStore.put(cursor.value);
+        cursor.continue();
+      }
+      transaction.oncomplete = function () {
+        resolve(true)
+      }
+      transaction.onerror = function () {
+        reject(transaction.error)
+      }
+    }
+    req.onerror = function () {
+      reject(req.error)
+    }
+  })
+}
