4bfc1020c4eba56b5701ae1418d564ceeeb368f9
shikhar0507 <kap.shikhar@gmail.com>
2019-06-25 13:42:18 +0530

    roolback

--- src/assests/js/panel.js
+++ src/assests/js/panel.js
@@ -319,14 +319,6 @@ function convertToJSON(body) {
 		return;
 	};
 	jsonData.forEach(function(val){
-		jsonData.forEach(function (val) {
-			Object.keys(val).forEach(function(name){
-				if(record.attachment[name].type === 'number'){
-					val[name] = Number(val[name])
-				}
-			})
-			val.share = [];
-		})
 		val.share = [];	
 	})
 	if(body.template === 'office') {
