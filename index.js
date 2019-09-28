72340eb2b1ca0828b47346d0b64225be4fe8554d
shikhar0507 <kap.shikhar@gmail.com>
2019-09-28 14:54:07 +0530

    content change

--- index.js
+++ index.js
@@ -7,32 +7,39 @@ import {
     login,
     updateAuth
 } from './js/login';
-import {home} from './js/home';
+import {
+    home
+} from './js/home';
 
 window.addEventListener('load', function () {
     firebase.initializeApp(appKeys.getKeys());
     firebase.auth().onAuthStateChanged(function (auth) {
-        redirect('/signup.html');
-        // console.log(auth);
-        // if(!auth) {
 
-        //     if (parseRedirect('redirect_to') === 'LOGIN') {
-              
-        //         login();
-        //         return;
-        //     };
-            
-        //     return redirect('/static/home.html');
-        // }
+        console.log(auth);
         
+        if (!auth) {
+            if (parseRedirect('redirect_to') === 'LOGIN') {
+                login();              
+                return;
+            };
+            if(parseRedirect('redirect_to') === 'SIGNUP'){
+                return  redirect('/signup.html');
+            }
+
+            return redirect('/static/home.html');           
+        }
+
+        // redirect('/static/home.html');           
+        return redirect('/signup.html');   
+
         // if (auth.email && auth.emailVerified && auth.displayName) {
         //     auth.getIdTokenResult().then((idTokenResult) => {
-        //         redirect('/signup.html');
-        //         if (idTokenResult.claims.hasOwnProperty('admin') && idTokenResult.claims.admin.length) return home(auth)
+        //         // redirect('/signup.html');
+        //         // if (idTokenResult.claims.hasOwnProperty('admin') && idTokenResult.claims.admin.length) return home(auth)
         //     });
         //     return;
         // };
-        
+
         // updateAuth(auth);
     });
 });
