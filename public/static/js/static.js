695ff95ce2dc43a420e0445e638d5a46ab030445
shikhar0507 <kap.shikhar@gmail.com>
2019-10-11 16:04:10 +0530

    Added navigation header

--- public/static/js/static.js
+++ public/static/js/static.js
@@ -1,30 +1,75 @@
 window.mdc.autoInit();
-const navigationDrawer = new mdc.tabBar.MDCTabBar(document.querySelector('#mobile-navigation-drawer .mdc-tab-bar'))
-navigationDrawer.listen('MDCTabBar:activated', function (e) {
-    console.log(e);
-    [].map.call(document.querySelectorAll('.tab-content-drawer'), function (el) {
-        el.classList.add('hidden');
-        document.getElementById('tab-content-drawer-' + e.detail.index).classList.remove(
-            'hidden');
-    })
-});
+// const navigationDrawer = new mdc.tabBar.MDCTabBar(document.querySelector('#mobile-navigation-drawer .mdc-tab-bar'))
+// navigationDrawer.listen('MDCTabBar:activated', function (e) {
+//     console.log(e);
+//     [].map.call(document.querySelectorAll('.tab-content-drawer'), function (el) {
+//         el.classList.add('hidden');
+//         document.getElementById('tab-content-drawer-' + e.detail.index).classList.remove(
+//             'hidden');
+//     })
+// });
 
-navigationDrawer.activateTab(0);
+// navigationDrawer.activateTab(0);
 
 
 const menu = new mdc.iconButton.MDCIconButtonToggle(document.getElementById('menu'))
 menu.listen('MDCIconButtonToggle:change', function (event) {
-    toggleNavigationDrawer(event.detail.isOn)
+    if(event.detail.isOn) {
+        return createHeaderNavigation()
+    }
+    return closeHeaderNavigaton()
 })
 
-function toggleNavigationDrawer(open){
-    if(open) {
-        // document.getElementById('mobile-navigation-drawer').classList.remove("hidden");
-        return
-    }
-    // document.getElementById('mobile-navigation-drawer').classList.add("hidden");
-}
 
 function getRandomInt(max) {
     return Math.floor(Math.random() * Math.floor(max));
+}
+
+document.getElementById('products-btn').addEventListener('click',createHeaderNavigation)
+document.getElementById('close-navigation-btn').addEventListener('click',closeHeaderNavigaton);
+function closeHeaderNavigaton() {
+    const el = document.querySelector(".mdc-top-app-bar--navigation");
+    el.classList.add('hidden')
+    // document.querySelector('.page-content-section').style.display = 'block';
+}
+function createHeaderNavigation(){
+    const el = document.querySelector(".mdc-top-app-bar--navigation");
+    el.classList.remove('hidden')
+    // document.querySelector('.page-content-section').style.display = 'none';
+
+    // if(window.screenWidth > 839) {
+    //     //if desktop
+    //     return desktopNavigationContent();
+    // }
+    // return mobileNavigationContent();
+}
+
+function desktopNavigationContent(){
+
+}
+
+function mobileNavigationContent() {
+    return `<ul class="mdc-list">
+    <a class="mdc-list-item" href="./attendace.html">
+        Attendance
+        <span class="mdc-list-item__meta material-icons">arrow_right</span>
+    </a>
+    <a class="mdc-list-item" href="./staff.html">
+        Staff allocation
+        <span class="mdc-list-item__meta material-icons">arrow_right</span>
+    </a>
+    <a class="mdc-list-item" href="./salary.html">
+        Salary & Compliance
+        <span class="mdc-list-item__meta material-icons">arrow_right</span>
+    </a>
+    <a class="mdc-list-item" href="./reimbursements.html">
+        Reimbursement
+        <span class="mdc-list-item__meta material-icons">arrow_right</span>
+    </a>
+    <a class="mdc-list-item" href="./reimbursements.html">
+        In-built payment transfer
+        <span class="mdc-list-item__meta material-icons">arrow_right</span>
+    </a>
+</ul>
+`
 }
