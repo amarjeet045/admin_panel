e384376f4377f6626f8d746608598994f554f914
shikhar0507 <kap.shikhar@gmail.com>
2019-10-17 16:06:28 +0530

    changed header,static pages and added app demo and getting started pages

--- public/static/js/static.js
+++ public/static/js/static.js
@@ -1,20 +1,26 @@
 window.mdc.autoInit();
-// const navigationDrawer = new mdc.tabBar.MDCTabBar(document.querySelector('#mobile-navigation-drawer .mdc-tab-bar'))
-// navigationDrawer.listen('MDCTabBar:activated', function (e) {
-//     console.log(e);
-//     [].map.call(document.querySelectorAll('.tab-content-drawer'), function (el) {
-//         el.classList.add('hidden');
-//         document.getElementById('tab-content-drawer-' + e.detail.index).classList.remove(
-//             'hidden');
-//     })
-// });
 
-// navigationDrawer.activateTab(0);
 
 
 const menu = new mdc.iconButton.MDCIconButtonToggle(document.getElementById('menu'))
 menu.listen('MDCIconButtonToggle:change', function (event) {
-    if(event.detail.isOn) {
+    if (event.detail.isOn) {
+        const navigationDrawer = new mdc.tabBar.MDCTabBar(document.getElementById('mobile-navigation-tab'))
+        navigationDrawer.listen('MDCTabBar:activated', function (e) {
+            console.log(e);
+            [].map.call(document.querySelectorAll('.mdc-top-app-bar--navigation-content-list'), function (el) {
+                el.classList.add('hidden');
+            });
+
+            if (e.detail.index == 0) {
+                document.getElementById('product-list-tab').classList.remove('hidden')
+                return
+            }
+            if (e.detail.index == 1) {
+                document.getElementById('company-list-tab').classList.remove('hidden')
+            }
+        });
+        navigationDrawer.activateTab(0);
         return createHeaderNavigation()
     }
     return closeHeaderNavigaton()
@@ -25,51 +31,31 @@ function getRandomInt(max) {
     return Math.floor(Math.random() * Math.floor(max));
 }
 
-document.getElementById('products-btn').addEventListener('click',createHeaderNavigation)
-document.getElementById('close-navigation-btn').addEventListener('click',closeHeaderNavigaton);
+document.getElementById('products-btn').addEventListener('click', showProductNav)
+document.getElementById('company-btn').addEventListener('click', showCompanyNav)
+document.getElementById('close-navigation-btn').addEventListener('click', closeHeaderNavigaton);
+
 function closeHeaderNavigaton() {
     const el = document.querySelector(".mdc-top-app-bar--navigation");
     el.classList.add('hidden')
     // document.querySelector('.page-content-section').style.display = 'block';
 }
-function createHeaderNavigation(){
+
+function showProductNav() {
+    createHeaderNavigation()
+    document.querySelector('.desktop-navigation .products').classList.remove('hidden');
+    document.querySelector('.desktop-navigation .company').classList.add('hidden');
+
+
+}
+
+function showCompanyNav() {
+    createHeaderNavigation()
+    document.querySelector('.desktop-navigation .company').classList.remove('hidden');
+    document.querySelector('.desktop-navigation .products').classList.add('hidden');
+}
+
+function createHeaderNavigation() {
     const el = document.querySelector(".mdc-top-app-bar--navigation");
     el.classList.remove('hidden')
-    // document.querySelector('.page-content-section').style.display = 'none';
-
-    // if(window.screenWidth > 839) {
-    //     //if desktop
-    //     return desktopNavigationContent();
-    // }
-    // return mobileNavigationContent();
-}
-
-function desktopNavigationContent(){
-
-}
-
-function mobileNavigationContent() {
-    return `<ul class="mdc-list">
-    <a class="mdc-list-item" href="./attendace.html">
-        Attendance
-        <span class="mdc-list-item__meta material-icons">arrow_right</span>
-    </a>
-    <a class="mdc-list-item" href="./staff.html">
-        Staff allocation
-        <span class="mdc-list-item__meta material-icons">arrow_right</span>
-    </a>
-    <a class="mdc-list-item" href="./salary.html">
-        Salary & Compliance
-        <span class="mdc-list-item__meta material-icons">arrow_right</span>
-    </a>
-    <a class="mdc-list-item" href="./reimbursements.html">
-        Reimbursement
-        <span class="mdc-list-item__meta material-icons">arrow_right</span>
-    </a>
-    <a class="mdc-list-item" href="./reimbursements.html">
-        In-built payment transfer
-        <span class="mdc-list-item__meta material-icons">arrow_right</span>
-    </a>
-</ul>
-`
 }
