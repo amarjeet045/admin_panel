274c70c575fd6ef44c52af49a835ff892c4be789
shikhar0507 <kap.shikhar@gmail.com>
2019-11-05 15:53:59 +0530

    Home page top bar changes

--- public/js/static.js
+++ public/js/static.js
@@ -1,61 +1,25 @@
 window.mdc.autoInit();
 
-
-
-const menu = new mdc.iconButton.MDCIconButtonToggle(document.getElementById('menu'))
-menu.listen('MDCIconButtonToggle:change', function (event) {
-    if (event.detail.isOn) {
-        const navigationDrawer = new mdc.tabBar.MDCTabBar(document.getElementById('mobile-navigation-tab'))
-        navigationDrawer.listen('MDCTabBar:activated', function (e) {
-            console.log(e);
-            [].map.call(document.querySelectorAll('.mdc-top-app-bar--navigation-content-list'), function (el) {
-                el.classList.add('hidden');
-            });
-
-            if (e.detail.index == 0) {
-                document.getElementById('product-list-tab').classList.remove('hidden')
-                return
-            }
-            if (e.detail.index == 1) {
-                document.getElementById('company-list-tab').classList.remove('hidden')
-            }
-        });
-        navigationDrawer.activateTab(0);
-        return createHeaderNavigation()
-    }
-    return closeHeaderNavigaton()
+const featuresBtn = document.getElementById('features-button');
+const featuresMenu = new mdc.menu.MDCMenu(document.getElementById('features-menu'));
+featuresBtn.addEventListener('click',function(){
+    featuresMenu.open = true;
 })
+
+const solutionsBtn = document.getElementById('solutions-button');
+const solutionsMenu = new mdc.menu.MDCMenu(document.getElementById('solutions-menu'));
+solutionsBtn.addEventListener('click',function(){
+    solutionsMenu.open = true;
+})
+
+
+// const menu = new mdc.iconButton.MDCIconButtonToggle(document.getElementById('menu'))
+// menu.listen('MDCIconButtonToggle:change', function (event) {
+
+// })
 
 
 function getRandomInt(max) {
     return Math.floor(Math.random() * Math.floor(max));
 }
 
-document.getElementById('products-btn').addEventListener('click', showProductNav)
-document.getElementById('company-btn').addEventListener('click', showCompanyNav)
-document.getElementById('close-navigation-btn').addEventListener('click', closeHeaderNavigaton);
-
-function closeHeaderNavigaton() {
-    const el = document.querySelector(".mdc-top-app-bar--navigation");
-    el.classList.add('hidden')
-    // document.querySelector('.page-content-section').style.display = 'block';
-}
-
-function showProductNav() {
-    createHeaderNavigation()
-    document.querySelector('.desktop-navigation .products').classList.remove('hidden');
-    document.querySelector('.desktop-navigation .company').classList.add('hidden');
-
-
-}
-
-function showCompanyNav() {
-    createHeaderNavigation()
-    document.querySelector('.desktop-navigation .company').classList.remove('hidden');
-    document.querySelector('.desktop-navigation .products').classList.add('hidden');
-}
-
-function createHeaderNavigation() {
-    const el = document.querySelector(".mdc-top-app-bar--navigation");
-    el.classList.remove('hidden')
-}
