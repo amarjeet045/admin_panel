3e330c46f6f10ea47df3d8a51842f30fef28c9e1
shikhar0507 <kap.shikhar@gmail.com>
2019-11-11 17:14:15 +0530

    updated images in featuresc

--- public/js/init.js
+++ public/js/init.js
@@ -5,25 +5,25 @@ window.addEventListener('load', function () {
     firebase.auth().onAuthStateChanged(user => {
       console.log(user)
       if (!user) {
-        if (parseRedirect('redirect_to') === 'LOGIN') {
-          login('app');
-          return;
-        };
+        
+        // if (parseRedirect('redirect_to') === 'LOGIN') {
+          // login('app');
+        //   return;
+        // };
         return redirect('/home.html');
       };
 
       if (user.email && user.emailVerified && user.displayName) {
         user.getIdTokenResult().then((idTokenResult) => {
           if (idTokenResult.claims.hasOwnProperty('admin') && idTokenResult.claims.admin.length) {
-            if (parseRedirect('redirect_to') === 'LOGIN') {
+            // if (parseRedirect('redirect_to') === 'LOGIN') {
               
-              history.pushState(null, null, window.location.pathname);
+            //   history.pushState(null, null, window.location.pathname);
               
-            }
+            // }
             getLocation().then(geopoint => {
                 return initializer(user,geopoint)
             }).catch(error => {
-               
                 initializer(user);
             })
             return;
