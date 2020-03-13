ac9c40825c51e057349ba879223cd81bca295114
shikhar0507 <kap.shikhar@gmail.com>
2020-03-13 15:50:38 +0530

    Updated office flow

--- public/js/init.js
+++ public/js/init.js
@@ -1,70 +1,67 @@
-function initializeLogIn(el,shouldRedirect = true,profileInfo) {
+function initializeLogIn(el, shouldRedirect = true, profileInfo) {
   var appKeys = new AppKeys();
   firebase.auth().onAuthStateChanged(user => {
     console.log(user)
-    
+
     if (!user) {
       document.body.classList.remove('hidden')
-      if(shouldRedirect) {
-          redirect('');
-          return;
+      if (shouldRedirect) {
+        redirect('');
+        return;
       }
-      login(el,profileInfo);
+      login(el, profileInfo);
       return;
     };
-    
+
     addLogoutBtn();
-    
+
     const param = parseURL()
-    if (user.email && user.displayName){
-      user.getIdTokenResult().then((idTokenResult) => {
-          console.log(idTokenResult.claims)
-        if (idTokenResult.claims.admin || idTokenResult.claims.support) {
-            if (window.location.pathname === '/app') {
-              getLocation().then(initializer).catch(err => {
-                initializer();
-              })
-              return
-            }
-            redirect(`/app${window.location.search}`);
-          return;
-        };
-      
-        http('GET', `${appKeys.getBaseUrl()}/api/services/subscription/checkIn`).then(response => {
-          if(response.hasCheckInSubscription)  {
-            setFirebaseAnalyticsUserProperty("hasCheckin", "true");
-            signOut()
-            showSnacksApiResponse('Please use Growthfile app on your mobile to continue');
-            setTimeout(function(){
-              window.location.href = 'https://growthfile.page.link/naxz';
-            },2000)
-            return;
-          }
-          if(window.location.pathname === '/signup') {
-            getLocation().then(createOfficeInit).catch(console.error)
-            return;
-          }
-          if(param && param.get('action') === 'get-started') {
-            redirect('/signup?action=get-started');
+    user.getIdTokenResult().then((idTokenResult) => {
+      if (idTokenResult.claims.admin || idTokenResult.claims.support) {
+        if (user.email && user.displayName) {
+          if (window.location.pathname === '/app') {
+            getLocation().then(initializer).catch(err => {
+              initializer();
+            })
             return
           }
-          redirect('/signup');
-        })
-      }); 
-      return
-    }
-   
-    updateAuth(el,user,profileInfo);
+          redirect(`/app${window.location.search}`);
+          return;
+        }
+        updateAuth(el, user, profileInfo);
+        return
+      }
+      http('GET', `${appKeys.getBaseUrl()}/api/services/subscription/checkIn`).then(response => {
+        if (response.hasCheckInSubscription) {
+          setFirebaseAnalyticsUserProperty("hasCheckin", "true");
+          signOut()
+          showSnacksApiResponse('Please use Growthfile app on your mobile to continue');
+          setTimeout(function () {
+            window.location.href = 'https://growthfile.page.link/naxz';
+          }, 2000)
+          return;
+        }
+        if (window.location.pathname === '/signup') {
+          getLocation().then(createOfficeInit).catch(console.error)
+          return;
+        }
+        if (param && param.get('action') === 'get-started') {
+          redirect('/signup?action=get-started');
+          return
+        }
+        redirect('/signup');
+      })
+    });
   });
 }
 
-const addLogoutBtn = () =>{
+const addLogoutBtn = () => {
   const el = document.getElementById('app-bar-login');
-  if(!el) return;
+  if (!el) return;
   el.textContent = 'Log out';
   el.removeAttribute('href');
-  el.addEventListener('click',function(){
-    firebase.auth().signOut().then(function(){
+  el.addEventListener('click', function () {
+    firebase.auth().signOut().then(function () {
       redirect('')
     })
   })
--- public/js/start.js
+++ public/js/start.js
@@ -6,64 +6,73 @@ function isAdmin(idTokenResult) {
 }
 
 function createOfficeInit(geolocation) {
+
+    const auth = firebase.auth().currentUser;
+    const authProps = {
+        displayName: auth.displayName,
+        phoneNumber: auth.phoneNumber,
+        email: auth.email
+    }
+
     const template = {
         'template': 'office',
-        'firstContact': '',
-        'secondContact': '',
+        'firstContact': authProps,
         'name': '',
-        'placeId': '',
         'registeredOfficeAddress': '',
-        canEdit: true
+        'canEdit': true
     }
     history.pushState(['addView'], null, null);
-    addView(document.getElementById('home-login'), template);
+    addView(document.getElementById('home-login'), template, authProps);
 
 }
+
+function handleAuthUpdate(authProps) {
+    const auth = firebase.auth().currentUser;
+    if (auth.displayName && auth.email) return;
+
+    if (!auth.displayName) {
+        auth.updateProfile({
+            displayName: authProps.displayName
+        }).then(console.log).catch(console.error)
+    }
+    if (!auth.email) {
+        emailUpdate(authProps.email, function () {
+            console.log('succesfully updated email')
+        })
+    }
+}
+
+
 
 function sendOfficeData(requestBody) {
-
-    var geocoder = new google.maps.Geocoder();
-    geocoder.geocode({
-        'address': requestBody.registeredOfficeAddress
-    }, function (geocodeResults, status) {
-        if (status == 'OK' && geocodeResults.length) {
-            requestBody.placeId = geocodeResults[0].place_id
-            const myNumber = firebase.auth().currentUser.phoneNumber
-            getLocation().then(function (geopoint) {
-                requestBody.geopoint = geopoint
-                http('POST', `${appKeys.getBaseUrl()}/api/services/office`, requestBody).then(function () {
-                    fbq('trackCustom', 'Office Created')
-                    analyticsApp.logEvent('office_created', {
-                        item_location_id: requestBody.placeId,
-                    })
-
-                    const fc = requestBody['firstContact'].phoneNumber;
-                    const sc = requestBody['secondContact'].phoneNumber;
-                    if (fc === myNumber || sc === myNumber) {
-                        window.location.reload();
-                        return;
-                    }
-                    try {
-                        document.getElementById('home-login').innerHTML = `<h3 class='mdc-typography--headline4 mdc-theme--primary'>${requestBody.name} Created</p>`;
-
-                    } catch (e) {
-
-                    }
-                }).catch(function (error) {
-                    showSnacksApiResponse(error.message);
-                })
-            }).catch(handleLocationError);
-            return
-        }
-    })
-
-}
-
-function sendUsersData(formData) {
+    const auth = firebase.auth().currentUser;
+    handleAuthUpdate(requestBody.auth);
+    const officeBody = requestBody.office;
     getLocation().then(function (geopoint) {
-        requestCreator('checkIns', formData, geopoint).then(function (response) {}).catch(console.error);
+        officeBody.geopoint = geopoint
+        return http('POST', `${appKeys.getBaseUrl()}/api/services/office`, officeBody).then(function () {
+            fbq('trackCustom', 'Office Created')
+            analyticsApp.logEvent('office_created', {
+                location: officeBody.registeredOfficeAddress
+            })
+            return http('POST', `${appKeys.getBaseUrl()}/api/services/subscription`, {
+                "share": [{
+                    phoneNumber: auth.phoneNumber,
+                    displayName: requestBody.auth.displayName,
+                    email: requestBody.auth.email
+                }],
+                "template": "subscription",
+                "office": officeBody.name,
+                geopoint:geopoint
+            })
+        }).then(function () {
+            window.location.reload();
+        }).catch(function (error) {
+            showSnacksApiResponse(error.message);
+        });
     }).catch(handleLocationError);
 }
+
 
 function sendSubscriptionData(formData) {
     getLocation().then(function (geopoint) {
