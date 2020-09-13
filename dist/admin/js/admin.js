664059bba1598d481885cb4cbdf56721d753899c
shikhar0507 <kap.shikhar@gmail.com>
2020-09-13 07:39:37 +0530

    fixed dom element missing for auth in signup page

---
+++ dist/admin/js/admin.js
@@ -0,0 +1,113 @@
+var init = function init(office, officeId) {
+  console.log('home page');
+  handleProfileDetails(officeId);
+  handleUserDetails(officeId);
+  handleLocationsDetails(officeId);
+};
+
+var handleUserDetails = function handleUserDetails(officeId) {
+  window.database.transaction("users").objectStore("users").getAll(null, 5).onsuccess = function (event) {
+    var records = event.target.result;
+
+    if (records.length) {
+      window.database.transaction("meta").objectStore("meta").get("meta").onsuccess = function (e) {
+        updateUsersSection(records, e.target.result.totalCheckedinUsers, e.target.result.totalUsersSize);
+      };
+    }
+
+    getUsersDetails(officeId, 5).then(function (response) {
+      updateUsersSection(response.results, response.totalCheckedinUsers, response.size);
+    }).catch(console.error);
+  };
+};
+
+var updateUsersSection = function updateUsersSection(users, totalCheckedinUsers, totalSize) {
+  var activeCont = document.getElementById('employees-active-container');
+
+  if (totalCheckedinUsers !== undefined && totalSize !== undefined) {
+    activeCont.innerHTML = "".concat(totalCheckedinUsers, "/").concat(totalSize, " ");
+  }
+
+  var ul = document.getElementById('employees-list');
+  ul.innerHTML = '';
+  users.forEach(function (user) {
+    var li = createElement('li', {
+      className: 'mdc-list-item user-list'
+    });
+
+    if (user.latestCheckIn.location) {
+      li.classList.add('user-list--location');
+    }
+
+    li.innerHTML = "<span class=\"mdc-list-item__ripple\"></span>\n        <img class='mdc-list-item__graphic' src=\"".concat(user.photoURL || '../img/person.png', "\">\n        <span class=\"mdc-list-item__text\">\n          <span class=\"mdc-list-item__primary-text\">").concat(user.displayName || user.phoneNumber, "</span>\n          <span class='mdc-list-item__meta list-time'>").concat(formatCreatedTime(user.latestCheckIn.timestamp), "</span>\n          <span class=\"mdc-list-item__secondary-text\">").concat(user.phoneNumber, "</span>\n          <span class=\"mdc-list-item__secondary-text\">").concat(user.latestCheckIn.location || '', "</span>\n        </span>");
+    new mdc.ripple.MDCRipple(li);
+    ul.appendChild(li);
+  });
+};
+
+var handleLocationsDetails = function handleLocationsDetails(officeId) {
+  window.database.transaction("locations").objectStore("locations").getAll(null, 5).onsuccess = function (event) {
+    var records = event.target.result;
+
+    if (records.length) {
+      window.database.transaction("meta").objectStore("meta").get("meta").onsuccess = function (e) {
+        updateLocationsSection(records, e.target.result.totalActiveLocations, e.target.result.totalLocationsSize);
+      };
+    }
+
+    http('GET', "".concat(appKeys.getBaseUrl(), "/api/office/").concat(officeId, "/location?limit=5&start=0")).then(function (response) {
+      var tx = window.database.transaction(["locations", "meta"], "readwrite");
+
+      for (var index = 0; index < response.results.length; index++) {
+        var result = response.results[index];
+        result['search_key'] = result.location ? result.location.toLowerCase() : null;
+        var locationStore = tx.objectStore("locations");
+        locationStore.put(result);
+      }
+
+      var metaStore = tx.objectStore("meta");
+
+      metaStore.get("meta").onsuccess = function (e) {
+        var metaData = e.target.result;
+        metaData.totalLocationsSize = response.size;
+        metaData.totalActiveLocations = response.totalActiveLocations;
+        metaStore.put(metaData);
+      };
+
+      tx.oncomplete = function () {
+        updateLocationsSection(response.results, response.totalActiveLocations, response.size);
+      };
+    }).catch(console.error);
+  };
+};
+
+var updateLocationsSection = function updateLocationsSection(locations, totalActiveLocations, totalSize) {
+  var activeCont = document.getElementById('locations-active-container');
+  var ul = document.getElementById('locations-list');
+
+  if (totalActiveLocations !== undefined && totalSize !== undefined) {
+    activeCont.innerHTML = "".concat(totalActiveLocations, "/").concat(totalSize, " ");
+  }
+
+  ul.innerHTML = '';
+  locations.forEach(function (location) {
+    var li = createElement('li', {
+      className: 'mdc-list-item user-list'
+    });
+    li.innerHTML = "<span class=\"mdc-list-item__ripple\"></span>\n        <span class=\"mdc-list-item__text\">\n          <span class=\"mdc-list-item__primary-text\">".concat(location['location'], "</span>\n          <span class=\"mdc-list-item__secondary-text\">").concat(location['address'], "</span>\n        </span>\n        <span class='mdc-list-item__meta list-time'>").concat(formatCreatedTime(location.timestamp), "</span>");
+    new mdc.ripple.MDCRipple(li);
+    ul.appendChild(li);
+  });
+};
+
+var formatCreatedTime = function formatCreatedTime(timestamp) {
+  if (!timestamp) return '';
+  return moment(timestamp).calendar(null, {
+    sameDay: 'hh:mm A',
+    lastDay: '[Yesterday]',
+    nextDay: '[Tomorrow]',
+    nextWeek: 'dddd',
+    lastWeek: 'DD/MM/YY',
+    sameElse: 'DD/MM/YY'
+  });
+};
---
+++ dist/admin/js/utils.js
@@ -0,0 +1,152 @@
+/** callback is used because activity returned by this function needs to update dom 2 times */
+var getCompanyDetails = function getCompanyDetails(officeId, onSuccess, onError) {
+  window.database.transaction("activities").objectStore("activities").get(officeId).onsuccess = function (event) {
+    var record = event.target.result;
+
+    if (record) {
+      onSuccess(record);
+    }
+
+    http('GET', "".concat(appKeys.getBaseUrl(), "/api/office/").concat(officeId, "/activity/").concat(officeId, "/")).then(function (officeActivity) {
+      window.database.transaction("activities", "readwrite").objectStore("activities").put(officeActivity);
+      onSuccess(officeActivity);
+    }).catch(onError);
+  };
+};
+
+var handleProfileDetails = function handleProfileDetails(officeId) {
+  getCompanyDetails(officeId, updateCompanyProfile, console.error);
+};
+
+var updateCompanyProfile = function updateCompanyProfile(activity) {
+  var companyLogo = document.getElementById('company-logo');
+  var companyName = document.getElementById('company-name');
+  var companyAddress = document.getElementById('company-address');
+  var companyDescription = document.getElementById('company-description');
+  var companyCategory = document.getElementById('company-category');
+
+  if (activity.attachment['Company Logo'].value) {
+    companyLogo.src = activity.attachment['Company Logo'].value;
+  }
+
+  companyName.textContent = activity.attachment['Name'].value;
+  companyAddress.textContent = activity.attachment['Registered Office Address'].value;
+  companyDescription.textContent = activity.attachment['Description'].value;
+  companyCategory.textContent = activity.attachment['Category'] ? activity.attachment['Category'].value : '';
+  document.querySelector('.mdc-drawer-app-content').classList.remove('initializing-db');
+
+  if (document.querySelector('.initializing-box')) {
+    document.querySelector('.initializing-box').remove();
+  }
+
+  if (!officeHasMembership(activity.schedule)) {
+    activity.geopoint = {
+      latitude: 0,
+      longitude: 0
+    };
+    http('PUT', "".concat(appKeys.getBaseUrl(), "/api/activities/update"), activity).then(function (res) {
+      var dialog = new mdc.dialog.MDCDialog(document.getElementById('payment-dialog'));
+      var dialogBody = document.getElementById('payment-dialog--body');
+      dialog.scrimClickAction = "";
+
+      if (activity.attachment['First Contact'].value === firebase.auth().currentUser.phoneNumber) {
+        dialog.open();
+        return;
+      }
+
+      dialogBody.innerHTML = 'Please ask the business owner to complete the payment';
+      dialog.open();
+    });
+  }
+};
+
+var getUsersDetails = function getUsersDetails(officeId, limit) {
+  return new Promise(function (resolve, reject) {
+    var url = "".concat(appKeys.getBaseUrl(), "/api/office/").concat(officeId, "/user");
+
+    if (limit) {
+      url = "".concat(appKeys.getBaseUrl(), "/api/office/").concat(officeId, "/user?limit=").concat(limit, "&start=0");
+    }
+
+    http('GET', url).then(function (response) {
+      var tx = window.database.transaction(["users", "meta"], "readwrite");
+
+      for (var index = 0; index < response.results.length; index++) {
+        var result = response.results[index];
+        result['search_key'] = result.displayName ? result.displayName.toLowerCase() : null;
+        var usersStore = tx.objectStore("users");
+        usersStore.put(result);
+      }
+
+      var metaStore = tx.objectStore("meta");
+
+      metaStore.get("meta").onsuccess = function (e) {
+        var metaData = e.target.result;
+        metaData.totalUsersSize = response.size;
+        metaData.totalCheckedinUsers = response.totalCheckedinUsers;
+        metaStore.put(metaData);
+      };
+
+      tx.oncomplete = function () {
+        resolve(response);
+      };
+    }).catch(reject);
+  });
+};
+/**
+ * format string to INR 
+ * @param {string} money 
+ * @returns {string} 
+ */
+
+
+var formatMoney = function formatMoney(money) {
+  var number = Number(money.split(',').join(''));
+  return number.toLocaleString('en-IN', {
+    style: 'currency',
+    currency: 'INR'
+  });
+};
+
+var emptyCard = function emptyCard(text) {
+  var div = createElement('div', {
+    className: 'mdc-card mdc-card--outlined  empty-card'
+  });
+  div.innerHTML = "<span class='text-cont'>\n    <i class='material-icons'>info</i>\n    ".concat(text, "\n    </span>");
+  return div;
+};
+
+var openProfileBox = function openProfileBox(event) {
+  event.stopPropagation();
+  document.querySelector('.user-profile--logo').classList.add('focused');
+  var el = document.querySelector('.profile-box');
+
+  if (el.classList.contains('hidden')) {
+    el.classList.remove('hidden');
+  } else {
+    closeProfileBox();
+  }
+
+  ;
+  var name = firebase.auth().currentUser.displayName;
+  var email = firebase.auth().currentUser.email;
+  var photo = firebase.auth().currentUser.photoURL;
+
+  if (photo) {
+    document.getElementById('auth-image').src = photo;
+  }
+
+  document.getElementById('auth-name').textContent = name;
+  document.getElementById('auth-email').textContent = email;
+};
+
+var closeProfileBox = function closeProfileBox() {
+  var el = document.querySelector('.profile-box');
+  document.querySelector('.user-profile--logo').classList.remove('focused');
+  el.classList.add('hidden');
+};
+
+var formSubmittedSuccess = function formSubmittedSuccess(button, text) {
+  button.classList.remove('active');
+  showSnacksApiResponse(text);
+};
