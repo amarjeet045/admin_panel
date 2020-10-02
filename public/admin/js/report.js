531137e05ac1e49c40e98c6f85712964cb918d21
shikhar0507 <kap.shikhar@gmail.com>
2020-10-02 21:26:58 +0530

    code polishing

--- public/admin/js/report.js
+++ public/admin/js/report.js
@@ -3,37 +3,30 @@ const reportStart = 06; // july
 const reportEnd = new Date().getMonth(); // current month
 const months = ['January', 'Febuary', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
 
+const yellowFill = {
+    type: 'pattern',
+    pattern: 'solid',
+    fgColor: {
+        argb: 'FFFFFF00'
+    },
+
+};
+const redFill = {
+    type: 'pattern',
+    pattern: 'solid',
+    fgColor: {
+        argb: 'FFFF0000'
+    },
+};
+
+const font = {
+    name: 'Arial',
+    bold: true
+}
+const CELL_WIDTH = 13.82;
+const CELL_HEIGHT = 15.5;
+
 const init = (office, officeId) => {
-
-    document.getElementById('file').addEventListener('change', (ev) => {
-        const fileList = ev.target.files;
-        const workbook = new ExcelJS.Workbook();
-        const reader = new FileReader()
-
-        reader.readAsArrayBuffer(fileList[0])
-        reader.onload = function () {
-            const buffer = reader.result;
-            workbook.xlsx.load(buffer).then(wbs => {
-                console.log(wbs.worksheets[0])
-                console.log(wbs.worksheets[0].getCell('H5'))
-
-            })
-        }
-        // workbook.xlsx.readFile(fileList[0]).then((data) => {
-        //     // var worksheet = workbook.getWorksheet();
-        //     // for (var v = 1; v <= worksheet.actualRowCount; v++) {
-        //     //     var lN = worksheet.getCell("B" + v).value;
-        //     //     console.log(" V :" + v + "------ Name :" + lN);
-        //     // }
-        //     console.log(workbook)
-        // })
-    })
-
-
-
-
-
-
 
     reportCards.forEach(card => {
         const btn = card.querySelector('.download-report-btn')
@@ -46,10 +39,6 @@ const init = (office, officeId) => {
         btn.addEventListener('click', (ev) => {
             btn.classList.add('in-progress');
 
-
-
-
-
             // return
 
             // http('GET', `${appKeys.getBaseURl()}/api/office/${officeId}/attendance/?name=MTD&month=${monthSelect.value}&year=2020`).then(res => {
@@ -58,6 +47,8 @@ const init = (office, officeId) => {
             // http('GET', 'https://us-central1-growthfilev2-0.cloudfunctions.net/api/office/gR0XF3YA03MA472QWkNp/attendance/?name=MTD&month=6&year=2020').then(res => {
             // console.log(res)
             // localStorage.setItem('res', JSON.stringify(res))
+
+
             let response = JSON.parse(localStorage.getItem('res'))
             const employees = {}
 
@@ -74,14 +65,18 @@ const init = (office, officeId) => {
                     showGridLines: true
                 }]
             });
+
+            /** set cell properties like width,height & font */
+            sheet.properties.defaultColWidth = CELL_WIDTH
+            sheet.properties.defaultRowHeight = CELL_HEIGHT
+
             sheet.mergeCells('A1:B1');
             sheet.getCell('A1').value = 'ATTENDANCE ' + moment(monthSelect.value).format('MM') + ' 2020'
-            // sheet.getCell('A2').value = 'EMP NAME'
-            // sheet.getCell('B2').value = 'EMP NO'
+            sheet.getCell('A1').alignment = {
+                horizontal: 'center'
+            }
 
-
-
-            // console.log(sheet)
+            /** start cell filling operation */
             let startRowIndex = 3;
             let endRowIndex = 5;
 
@@ -122,6 +117,9 @@ const init = (office, officeId) => {
                     sheet.mergeCells(1, startRowIndex, 1, endRowIndex)
                     const column = sheet.getColumn(startRowIndex)
                     column.header = date
+                    column.alignment = {
+                        horizontal: 'center'
+                    }
                     startRowIndex = endRowIndex + 1
                     endRowIndex += 3
 
@@ -131,122 +129,64 @@ const init = (office, officeId) => {
             })
 
 
-            const notFullArr = [];
-            let col = 3;
-            let row = 3;
             console.log(employees)
             Object.keys(employees).forEach(phoneNumber => {
                 const dateRangeArr = []
                 const subHeaders = []
                 const item = employees[phoneNumber]
-                row++
                 item.dates.forEach(date => {
 
                     subHeaders.push('start time', 'end time', 'hours')
-                    if (date.totalHours < 8) {
-                        notFullArr.push(`${sheet.getColumn(col).letter}${row}`, `${sheet.getColumn(col+1).letter}${row}`, `${sheet.getColumn(col+2).letter}${row}`)
-                    }
 
-                    col++
                     dateRangeArr.push(date.startTime, date.endTime, date.totalHours)
                 })
                 subHeaders.push('TOTAL DAYS', 'DAYS WORKED', 'TOTAL HOURS WORKED')
                 dateRangeArr.push(item.totalDays, item.totalDaysWorked, item.totalHoursWorked)
 
-                const newHead = sheet.addRow([...['EMP NAME', 'EMP NO'], ...subHeaders])
+                const newHead = sheet.addRow([...['EMP NAME', 'MOBILE NO'], ...subHeaders])
                 const newRow = sheet.addRow([...[item.employeeName, phoneNumber], ...dateRangeArr])
-                for (i = 5; i <= newRow.cellCount - 3; i += 3) {
-                    console.log(newRow.getCell(i).value)
-                    if (newRow.getCell(i).value == null || newRow.getCell(i).value == undefined) {
 
-                        sheet.mergeCells(newRow.getCell(i - 2).address, newRow.getCell(i).address)
-                        newRow.getCell(i - 2).value = 'ABSENT'
-
-                        newRow.getCell(i).fill = {
-                            type: 'pattern',
-                            pattern: 'darkVertical',
-                            fgColor: {
-                                argb: 'FFFF0000'
-                            },
-
-                        };
-
-                        newRow.getCell(i - 1).fill = {
-                            type: 'pattern',
-                            pattern: 'solid',
-                            fgColor: {
-                                argb: 'FFFF0000'
-                            },
-
-                        };
-                        newRow.getCell(i - 2).fill = {
-                            type: 'pattern',
-                            pattern: 'darkTrellis',
-                            fgColor: {
-                                argb: 'FFFF0000'
-                            },
-
-                        };
-                    } else if (newRow.getCell(i).value < 8) {
-                        newRow.getCell(i).fill = {
-                            type: 'pattern',
-                            pattern: 'darkVertical',
-                            fgColor: {
-                                argb: 'FFFFFF00'
-                            },
-                            bgColor: {
-                                argb: 'FFFFFF00'
-                            }
-                        };
-
-                        newRow.getCell(i - 1).fill = {
-                            type: 'pattern',
-                            pattern: 'solid',
-                            fgColor: {
-                                argb: 'FFFFFF00'
-                            },
-                            bgColor: {
-                                argb: 'FF0000FF'
-                            }
-                        };
-                        newRow.getCell(i - 2).fill = {
-                            type: 'pattern',
-                            pattern: 'darkTrellis',
-                            fgColor: {
-                                argb: 'FFFFFF00'
-                            },
-                            bgColor: {
-                                argb: 'FF0000FF'
-                            }
-                        };
-                    }
+                newHead.alignment = {
+                    horizontal: 'center'
+                }
+                newRow.alignment = {
+                    horizontal: 'center'
                 }
 
+                // start with first date  cell and find hour value
+                // if hours are not found mark date as absent
+                // if hours are less than 8 mark date as invalid (user didn't completed 8 hours)
 
+                for (i = 5; i <= newRow.cellCount - 3; i += 3) {
+                    const hourCell = newRow.getCell(i);
+                    const endTimeCell = newRow.getCell(i - 1);
+                    const startTimeCell = newRow.getCell(i - 2);
 
+                    if (newRow.getCell(i).value == null || newRow.getCell(i).value == undefined) {
+                        sheet.mergeCells(newRow.getCell(i - 2).address, newRow.getCell(i).address)
+                        hourCell.fill = redFill
+                        endTimeCell.fill = redFill
+                        startTimeCell.fill = redFill
+                        startTimeCell.value = 'ABSENT'
+                    } else if (newRow.getCell(i).value < 8) {
+                        hourCell.fill = yellowFill
+                        endTimeCell.fill = yellowFill
+                        startTimeCell.fill = yellowFill
+                    }
+                }
             })
 
-            sheet.getRow(1).font = {
-                name: 'Arial',
-                bold: true
-            }
-            sheet.getRow(1).alignment = {
-                horizontal: 'center'
-            }
-            sheet.getRow(2).font = {
-                name: 'Arial',
-                bold: true
-            }
-            sheet.getRow(2).alignment = {
-                horizontal: 'center'
-            }
+            sheet.getColumn(1).width = CELL_WIDTH
+            sheet.getColumn(2).width = CELL_HEIGHT
 
-            // const lastColumn = sheet.columns[sheet.columns.length - 1]
-            // sheet.getCell(lastColumn.letter + '2').value = 'TOTAL DAYS'
-            // sheet.getCell(sheet.getColumn(lastColumn.number + 1).letter + '2').value = 'DAYS WORKED'
-            // sheet.getCell(sheet.getColumn(lastColumn.number + 2).letter + '2').value = 'TOTAL HOURS WORKED'
+            sheet.getRow(1).font = font
+            sheet.getColumn(1).font = font
+            sheet.getRow(2).font = font
+            sheet.getColumn(2).font = font
 
             console.log(sheet)
+
+            /** Download workbook */
             workbook.xlsx.writeBuffer().then(data => {
                 console.log(data)
                 var blob = new Blob([data], {
@@ -260,11 +200,11 @@ const init = (office, officeId) => {
                 anchor.click();
                 window.URL.revokeObjectURL(url);
             })
-
-
         })
     })
 }
+
+
 const monthList = (month) => {
     const el = createElement('li', {
         className: 'mdc-list-item',
